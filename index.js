65536436d6903428a07be645fe9af7c7be6cdb5a
offstream <evangeline.niez@gmail.com>
2022-08-26 20:31:41 +0800

    don't forget to build!

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 "use strict"
 
-import('./output/Main/index.js').then(Main => {
+import("./output/Main/index.js").then(Main => {
   Main.main()
+}).catch(_ => {
+  console.error("Hey! You may have forgotten to build the thing:")
+  console.info("$ pnpm build")
 })
